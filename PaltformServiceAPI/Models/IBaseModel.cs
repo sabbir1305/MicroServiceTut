1877989d274cc7a051216ffea8abfc405f9703dd
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-09-25 23:17:15 +0600

    Model added

---
+++ PaltformServiceAPI/Models/IBaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaltformServiceAPI.Models
+{
+   public interface IBaseModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ PaltformServiceAPI/Models/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaltformServiceAPI.Models
+{
+    public class Platform : IBaseModel
+    {
+        [Key]
+        public int Id { get ; set ; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Publisher { get; set; }
+
+        [Required]
+        public string Cost { get; set; }
+
+    }
+}
